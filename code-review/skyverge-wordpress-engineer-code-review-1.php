ead2fde0841e6ecdba5665bdbf881ade390b4a9b
Marcos Rezende <rezehnde@gmail.com>
2019-10-30 07:51:04 -0300

    My version of the code

--- code-review/skyverge-wordpress-engineer-code-review-1.php
+++ code-review/skyverge-wordpress-engineer-code-review-1.php
@@ -6,44 +6,44 @@
  * == What changes would you suggest to reduce or remove that delay? ==
  */
 function add_my_courses_section() {
-	global $current_user;
+    global $current_user;
 
-	$api_user_id = get_user_meta( $current_user->ID, '_external_api_user_id', true );
+    $api_user_id = get_user_meta( $current_user->ID, '_external_api_user_id', true );
 
-	if ( !$api_user_id ) {
-		return;
-	}
+    if ( !$api_user_id ) {
+        return;
+    }
 
-	/**
-	 * I have removed the $this reference because this is not a class and create only one $api object
-	 * despite two of then as in the previous code
-	 */
+    /**
+     * I have removed the $this reference because this is not a class and create only one $api object
+     * despite two of then as in the previous code
+     */
     $api = get_api();
-	$courses  = $api->get_courses_assigned_to_user( $api_user_id );
-	$sso_link = $api->get_sso_link( $api_user_id );
+    $courses  = $api->get_courses_assigned_to_user( $api_user_id );
+    $sso_link = $api->get_sso_link( $api_user_id );
 
-	?>
-	<h2 style="margin-top: 40px;"><?php print __( 'My Courses', 'text-domain' ); ?></h2>
-	<table>
-		<thead><tr>
-			<th><?php echo __( 'Course Code', 'text-domain' ); ?></th>
-			<th><?php echo __( 'Course Title', 'text-domain' ); ?></th>
-			<th><?php echo __( 'Completion', 'text-domain' ); ?></th>
-			<th><?php echo __( 'Date Completed', 'text-domain' ); ?></th>
-		</tr></thead>
-		<tbody>
-		<?php
-		foreach( $courses as $course ) :
-			?><tr>
-			<td><?php echo __( $course['Code'] ); ?></td>
-			<td><?php echo __( $course['Name'] ); ?></td>
-			<td><?php echo __( $course['PercentageComplete'] ); ?> &#37;</td>
-			<td><?php echo __( $course['DateCompleted'] ); ?></td>
-		<?php endforeach;
-		?>
-		</tbody>
-	</table>
-	<p><a href="<?php echo $sso_link; ?>" target="_blank" class="button <?php echo $_GET['active_course']; ?>"><?php echo __( 'Course Login', 'text-domain' ); ?></a></p>
+    ?>
+    <h2 style="margin-top: 40px;"><?php print __( 'My Courses', 'text-domain' ); ?></h2>
+    <table>
+        <thead><tr>
+            <th><?php echo __( 'Course Code', 'text-domain' ); ?></th>
+            <th><?php echo __( 'Course Title', 'text-domain' ); ?></th>
+            <th><?php echo __( 'Completion', 'text-domain' ); ?></th>
+            <th><?php echo __( 'Date Completed', 'text-domain' ); ?></th>
+        </tr></thead>
+        <tbody>
+        <?php
+        foreach( $courses as $course ) :
+            ?><tr>
+            <td><?php echo __( $course['Code'] ); ?></td>
+            <td><?php echo __( $course['Name'] ); ?></td>
+            <td><?php echo __( $course['PercentageComplete'] ); ?> &#37;</td>
+            <td><?php echo __( $course['DateCompleted'] ); ?></td>
+        <?php endforeach;
+        ?>
+        </tbody>
+    </table>
+    <p><a href="<?php echo $sso_link; ?>" target="_blank" class="button <?php echo $_GET['active_course']; ?>"><?php echo __( 'Course Login', 'text-domain' ); ?></a></p>
 
-	<?php
+    <?php
 }
